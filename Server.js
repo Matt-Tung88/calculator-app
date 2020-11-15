fa8648913c76cc7eee4d2f2ee13b16cd358f5b2a
Matt-Tung88 <mttw23@gmail.com>
2020-11-14 20:28:05 -0500

    update

--- Server.js
+++ Server.js
@@ -5,10 +5,13 @@ const io = require("socket.io")(http);
 const path = require("path");
 
 
+
+
 //what port the web server to listen on
 const port = process.env.PORT || 3000;
 
 
+// app.use(express.static(path.join(__dirname, 'build')));
 app.use(express.static(path.join(__dirname, 'build')));
 
 //empty arrays: results = all the calculations for the new user to see and sockets = keep track of new/old socket.ids
@@ -22,11 +25,11 @@ io.on("connection", (socket) => {
 
 
   //if a new user goes to the app, then send the results array to client side
-  // if (ids.includes(socket.id) === false) {
-  //   io.to(socket.id).emit("new-user", results);
-  // } 
+  if (ids.includes(socket.id) === false) {
+    io.to(socket.id).emit("new-user", results);
+  } 
 
-  // ids.push(socket.id);
+  ids.push(socket.id);
 
   socket.on("disconnect", () => {
     console.log("User disconnected");
@@ -34,11 +37,11 @@ io.on("connection", (socket) => {
 
   //move each output into results array at the beginning of the array and make sure the length doesnt go exceed 10
   socket.on("output", (result) => {
-    // results.unshift(result);
+    results.unshift(result);
 
-    // if (results.length > 10) {
-    //   results.pop();
-    // }
+    if (results.length > 10) {
+      results.pop();
+    }
     //send result back to client so users can see other users' calculations
     io.emit("outputs", result);
 
